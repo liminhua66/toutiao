4cb07fd3b2c71aaf54ac67896c6c2d97beeb215d
liminhua66 <liminhua66@hotmail.com>
2022-07-31 11:22:58 +0800

    我的页面token切换

--- src/views/Profile/index.vue
+++ src/views/Profile/index.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>Profile</div>
+  <div>
+    <header>
+      <div class="login" v-if="isLogin">login</div>
+      <div class="logout" v-else>logout</div>
+    </header>
+    <main>收藏、历史、消息通知、小智同学</main>
+    <footer>
+      <button v-if="isLogin" @click="logout">退出</button>
+    </footer>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    isLogin() {
+      return !!this.$store.state.tokenObj.token
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.commit('SET_TOKEN', {})
+    }
+  }
+}
 </script>
 
 <style></style>
